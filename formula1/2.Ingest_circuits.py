0b2763f1489173aa699c00434ab4dd4c8e501912
abhijit.royace@gmail.com <abhijit.royace@gmail.com>
2021-11-24 14:09:55 +0000

    ingest of circuits file completed

--- formula1/2.Ingest_circuits.py
+++ formula1/2.Ingest_circuits.py
@@ -1,16 +1,25 @@
 # Databricks notebook source
-dbutils.fs.mounts()
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+from pyspark.sql.functions import col, current_timestamp,lit
 
 # COMMAND ----------
 
-dbutils.fs.ls("/mnt/abhijitstorageaccount32/raw")
+ circuits_schema = StructType(fields=[StructField('circuitId', IntegerType(), False),
+                                     StructField('circuitRef', StringType(), True),
+                                     StructField('name', StringType(), True),
+                                     StructField('location', StringType(), True),
+                                     StructField('country', StringType(), True),
+                                     StructField('lat', DoubleType(), True),
+                                     StructField('lang', DoubleType(), True),
+                                     StructField('alt', IntegerType(), True),
+                                     StructField('url', StringType(), True)])
 
 # COMMAND ----------
 
 circuits_df = spark.read\
 .option("header",True)\
-.option("inferSchema", True) \
-.csv("dbfs:/mnt/abhijitstorageaccount32/raw/circuits.csv")
+.schema(circuits_schema) \
+.csv("dbfs:/mnt/raw/circuits.csv")
 
 # COMMAND ----------
 
@@ -19,6 +28,49 @@ display(circuits_df)
 # COMMAND ----------
 
 circuits_df.printSchema()
+
+
+# COMMAND ----------
+
+circuits_selected_df =  circuits_df.select(col('circuitId'), col('circuitRef'), col('name'), col('location'), col('country'), col('lat'), col('lang'), col('alt'))
+
+# COMMAND ----------
+
+display(circuits_selected_df)
+
+# COMMAND ----------
+
+circuits_renamed_df =  circuits_selected_df.withColumnRenamed('circuitId', 'circuit_id') \
+.withColumnRenamed('circuitRef', 'circuit_ref') \
+.withColumnRenamed('lat', 'latitude') \
+.withColumnRenamed('lang', 'longitude') \
+.withColumnRenamed('alt', 'altitude') \
+        
+
+# COMMAND ----------
+
+display (circuits_renamed_df)
+
+# COMMAND ----------
+
+circuits_final_df = circuits_renamed_df.withColumn('ingestion_date', current_timestamp()) \
+.withColumn('environment',lit('production'))
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+circuits_final_df.write.mode('overwrite').parquet('mnt/processed/circuits')
+
+# COMMAND ----------
+
+dbutils.fs.ls('/mnt/processed/circuits')
+
+# COMMAND ----------
+
+display(spark.read.parquet('/mnt/processed/circuits'))
 
 # COMMAND ----------
 
