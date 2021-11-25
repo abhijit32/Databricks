814e5fcd594fd6865fb78be955b57d3bffbc5daa
abhijit.royaec@gmail.com <abhijit.royaec@gmail.com>
2021-11-25 21:40:23 +0000

    ingestion of the lap times data completed

---
+++ formula1/8.Ingest_lapTiimes.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType, DateType
+from pyspark.sql.functions import col, current_timestamp,lit, to_timestamp, concat
+
+# COMMAND ----------
+
+lapTime_schema = StructType(fields=[StructField('raceId', IntegerType(), False),
+                                    StructField('driverId', IntegerType(), False),
+                                    StructField('lap', IntegerType(), True),
+                                    StructField('position', IntegerType(), True),
+                                    StructField('time', StringType(), True),
+                                    StructField('milliseconds', IntegerType(), True)])
+
+# COMMAND ----------
+
+laptimes_df = spark.read.option('header',True).option('multiLine', True).schema(lapTime_schema).csv('/mnt/raw/lap_times')
+display(laptimes_df)
+
+# COMMAND ----------
+
+laptimes_final_df = laptimes_df.withColumnRenamed('raceId','race_id').withColumnRenamed('driverId','driver_id').withColumn('ingestion_date',current_timestamp())
+display(laptimes_final_df)
+
+# COMMAND ----------
+
+laptimes_final_df.write.mode('overwrite').parquet('mnt/processed/lap_times')
+
+# COMMAND ----------
+
+
